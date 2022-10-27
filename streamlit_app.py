47d9a5acfe24a0649f7a7efc15d3dd6adc4c8077
ot-babs <106931726+ot-babs@users.noreply.github.com>
2022-10-27 10:04:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit 
 
-streamlit.title('Myt Parents are the Best')
+streamlit.title('A Recommended Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('  🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
